9d36a8c00d11f38b775e6199029c4cba0dd9143f
David <dwviel@frosty2.ajdc.net>
2018-06-08 10:54:29 -0400

    modified tcpip_server to not fork child process, so no SIGCHILD sent to parent

--- tcpip_cli_srv/tcpip_server.c
+++ tcpip_cli_srv/tcpip_server.c
@@ -129,13 +129,13 @@ int main(void)
 			s, sizeof s);
 		printf("server: got connection from %s\n", s);
 
-		if (!fork()) { // this is the child process
-			close(sockfd); // child doesn't need the listener
+		//if (!fork()) { // this is the child process
+		//	close(sockfd); // child doesn't need the listener
 			if (send(new_fd, "Hello, world!", 13, 0) == -1)
 				perror("send");
 			close(new_fd);
-			exit(0);
-		}
+			//	exit(0);
+			//	}
 		close(new_fd);  // parent doesn't need this
 	}
 
