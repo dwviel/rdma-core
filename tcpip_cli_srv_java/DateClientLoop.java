7bd3de211a57d32f1e08fbee9293ff18a2a2aa9f
David <dwviel@frosty2.ajdc.net>
2018-06-13 11:09:34 -0400

    added a pair of cli and srv that loops continuously DateClientLoop and DateServerLoop

---
+++ tcpip_cli_srv_java/DateClientLoop.java
@@ -0,0 +1,62 @@
+package edu.lmu.cs.networking;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.InetAddress;
+import java.net.SocketAddress;
+import java.net.InetSocketAddress;
+
+import javax.swing.JOptionPane;
+
+/**
+ * Trivial client for the date server.
+ */
+public class DateClientLoop {
+
+    /**
+     * Runs the client as an application.  First it displays a dialog
+     * box asking for the IP address or hostname of a host running
+     * the date server, then connects to it and displays the date that
+     * it serves.
+     * See: cs.lmu.edu/~ray/notes/javanetexamples/
+     */
+    public static void main(String[] args) throws IOException {
+        //String serverAddress = JOptionPane.showInputDialog(
+         
+	System.out.println("Enter IP Address of a machine that is\n" +
+            "running the date service on port 9090:");
+	BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+	String serverAddress = reader.readLine();
+	String addrStr = "Server addr: " + serverAddress;
+	System.out.println(addrStr);
+
+	// create socket
+        Socket s = new Socket(serverAddress, 9090);
+
+	InetAddress inetAddr = s.getLocalAddress();
+	String inetAddrStr = inetAddr.getHostAddress();
+	String inetAddrStrOut = "InetAddr: " + inetAddrStr;
+	System.out.println(inetAddrStrOut);
+
+	InetSocketAddress remInetSockAddr = (InetSocketAddress)s.getRemoteSocketAddress();
+	String remInetAddr = remInetSockAddr.getHostString();
+	String remInetAddrStr = "Remote InetAddr: " + remInetAddr;
+	System.out.println(remInetAddrStr);
+
+	while(true)
+	{
+	    // read from socket
+	    BufferedReader input =
+		new BufferedReader(new InputStreamReader(s.getInputStream()));
+	    String answer = input.readLine();
+	    String recvStr = "Received Str: " + answer;
+	    System.out.println(recvStr);
+	}
+
+
+        //JOptionPane.showMessageDialog(null, answer);
+        //System.exit(0);
+    }
+}
---
+++ tcpip_cli_srv_java/DateServerLoop.java
@@ -0,0 +1,48 @@
+package edu.lmu.cs.networking;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * A TCP server that runs on port 9090.  When a client connects, it
+ * sends the client the current date and time, then closes the
+ * connection with that client.  Arguably just about the simplest
+ * server you can write.
+ */
+public class DateServerLoop {
+
+    /**
+     * Runs the server.
+     */
+    public static void main(String[] args) throws IOException 
+    {
+        ServerSocket listener = new ServerSocket(9090);
+        try 
+	{
+	    Socket socket = listener.accept();
+            while (true) 
+	    {
+		PrintWriter out =
+		    new PrintWriter(socket.getOutputStream(), true);
+		out.println(new Date().toString());
+
+		try        
+		{
+		    Thread.sleep(1000);  // 1000 ms
+		} 
+		catch(InterruptedException ex) 
+		{
+		    Thread.currentThread().interrupt();
+		}
+	    }
+	}
+        finally 
+	{
+            listener.close();
+	}
+    }
+}
